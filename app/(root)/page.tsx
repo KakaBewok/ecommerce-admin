f7a5430e716d386dd7b15b99ce6e3db26450901b
noprizal <noprizal@voltras.co.id>
2024-02-13 22:25:33 +0700

    fixing modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <Modal title="test" description="test" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 };
--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 interface ModalProps {
   title: string;
   description: string;
@@ -17,7 +25,19 @@ export const Modal: React.FC<ModalProps> = ({
 }) => {
   const onChange = (open: boolean) => {
     if (!open) {
-      onClose;
+      onClose();
     }
   };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
 };
