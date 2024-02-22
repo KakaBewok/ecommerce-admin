cbba4d27a5009d928dad3b7a6a94c2d8d3921a18
noprizal <noprizal@voltras.co.id>
2024-02-22 23:47:55 +0700

    setup nav

---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "@/components/main-nav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>this will be a store switcer</div>
+        <MainNav />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
