b0442772971a027725e7790a74dfc867fe4a17e6
noprizal <noprizal@voltras.co.id>
2024-03-02 12:06:27 +0700

    create setting page

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { TrashIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Form,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface SettingFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingFormValues = z.infer<typeof formSchema>;
+
+export const SettingForm: React.FC<SettingFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Setting" description="Manage store preference" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <TrashIcon className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,3 +1,38 @@
-export default function SettingPage() {
-  return <div>Settings page</div>;
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { SettingForm } from "./components/setting-form";
+
+interface SettingPageProps {
+  params: {
+    storeId: string;
+  };
 }
+
+const SettingPage: React.FC<SettingPageProps> = async ({ params }) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingPage;
