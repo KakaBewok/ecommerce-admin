0c2b75b3da1cd64e7a6b4063ee854c5d3585cde4
noprizal <noprizal@voltras.co.id>
2024-03-27 01:13:38 +0700

    set product page

--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -7,13 +7,13 @@ import { CellAction } from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type BillboardColumn = {
+export type ProductColumn = {
   id: string;
   label: string;
   createdAt: string;
 };
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<ProductColumn>[] = [
   {
     accessorKey: "label",
     header: ({ column }) => {
